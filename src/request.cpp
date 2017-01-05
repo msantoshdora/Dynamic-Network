182667ab35645fd5d846cc26265baa0560ba28db
Santosh Dora <sanal5hd@gmail.com>
2017-01-05 16:38:25 +0530

    Added deadline

--- src/request.cpp
+++ src/request.cpp
@@ -1,21 +1,24 @@
 #include "request.h"
+#include<bits/stdc++.h>
 
 
 //Default Constructor
 Request::Request(){ 
-	setRequest(0,0,0,0,0);
+	setRequest(0,0,0,0,0,0,0);
 }
 // Request constructor
-Request::Request(int src, int dest, double h_time, double bw,float sn){
+Request::Request(int src, int dest, double h_time, double bw,float sn,double dl,clock_t br){
 
-	setRequest(src,dest,h_time,bw,sn);
+	setRequest(src,dest,h_time,bw,sn,dl,br);
 
 }
 // Request member Function to set request
-void Request::setRequest(int src, int dest, double h_time, double bw, float sn){
+void Request::setRequest(int src, int dest, double h_time, double bw, float sn,double dl,clock_t br){
 	source = src;
 	destination = dest;
 	holding_time = h_time;
 	bandwidth = bw;
 	sinr = sn;
+	deadline = dl;
+	birth = br;
 }
