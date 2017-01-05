2740fda4dd28cca5d305e366140feba3ebfc9cbd
Santosh Dora <sanal5hd@gmail.com>
2017-01-05 16:38:12 +0530

    Added birth,deadline as parameters

--- src/request.h
+++ src/request.h
@@ -1,6 +1,9 @@
 #ifndef REQUEST_H
 #define REQUEST_H
 
+#include<ctime>
+#include<bits/stdc++.h>
+
 class Request{
 	public:
 		int source;
@@ -8,10 +11,12 @@ class Request{
 		double holding_time;
 		double bandwidth;
 		float sinr;
+		double deadline;
+		clock_t birth;
 	public:
 		Request();
-		Request(int src, int dest, double h_time, double bw,float sn);
-		void setRequest(int src, int dest, double h_time, double bw,float sn);
+		Request(int src, int dest, double h_time, double bw,float sn,double dl,clock_t br);
+		void setRequest(int src, int dest, double h_time, double bw,float sn, double dl,clock_t br);
 		
 };
 
